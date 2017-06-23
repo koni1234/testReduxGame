cbd9c03454100a8c16fd89c2dbdc66ffe7f2c5eb
Nicolò Andreozzi <andreozzi.nicolo@gmail.com>
2017-06-24 01:16:11 +0200

    react component su container game

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React , { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { gameInit, selectGame , selectGameMode , selectGameDifficult } from '../actions'
@@ -10,38 +10,41 @@ import GamesList from '../components/GamesList'
 import Board from '../components/Board'
 
 
-const Game = ({ games, selectedGame , selectedGameMode , selectedGameDifficult , selectGame , selectGameMode, selectGameDifficult, gameInit , gameStatus}) => {
-	
-	return (
-		<GamesList title="Games">
-		{games.map(game => {
-			let btns = [];
-			btns.push(<Button key="selectGame" disabled={(selectedGame === game.name) ? true : false} onClick={() => selectGame(game.name)} value="Select Game"/>);
-			if(selectedGame === game.name) {
-				btns.push(<Button key="getSelectedGameModeTime" disabled={(selectedGameMode && selectedGame === game.name) ? true : false} onClick={() => selectGameMode("time")} value="time" />);
-				btns.push(<Button key="getSelectedGameModeFree" disabled={(selectedGameMode && selectedGame === game.name) ? true : false} onClick={() => selectGameMode("free")} value="free" />);
-			}
-			if(selectedGame === game.name && selectedGameMode) {
-				btns.push(<Button key="getSelectedGameDifficultEasy" disabled={(selectedGameDifficult && selectedGame === game.name) ? true : false} onClick={() => selectGameDifficult("easy")} value="easy" />);
-				btns.push(<Button key="getSelectedGameDifficultMedium" disabled={(selectedGameDifficult && selectedGame === game.name) ? true : false} onClick={() => selectGameDifficult("medium")} value="medium" />);
-				btns.push(<Button key="getSelectedGameDifficultHard" disabled={(selectedGameDifficult && selectedGame === game.name) ? true : false} onClick={() => selectGameDifficult("hard")} value="hard" />);
-			}
-			if(selectedGame === game.name && selectedGameMode && selectedGameDifficult && gameStatus == "" ) {
-				gameInit(game)
-			}
-			
-			return (<GameItem
-					key={game.name}
-					name={game.name}
-					thumb={game.thumb}
-					selected={(selectedGame === game.name) ? true : false}
-				 >{btns}</GameItem>)
-			}
-		)}
-		</GamesList>
-	)
-}
+class Game extends Component {
+ 
+	render() {
+		const { games, selectGame , selectGameMode , selectGameDifficult , selectedGame, selectedGameMode , selectedGameDifficult , gameStatus } = this.props;
+		
+		return(
+			<GamesList title="Games">
+			{games.map(game => {
+				let btns = [];
+				btns.push(<Button key="selectGame" disabled={(selectedGame === game.name) ? true : false} onClick={() => selectGame(game.name)} value="Select Game"/>);
+				if(selectedGame === game.name) {
+					btns.push(<Button key="getSelectedGameModeTime" disabled={(selectedGameMode && selectedGame === game.name) ? true : false} onClick={() => selectGameMode("time")} value="time" />);
+					btns.push(<Button key="getSelectedGameModeFree" disabled={(selectedGameMode && selectedGame === game.name) ? true : false} onClick={() => selectGameMode("free")} value="free" />);
+				}
+				if(selectedGame === game.name && selectedGameMode) {
+					btns.push(<Button key="getSelectedGameDifficultEasy" disabled={(selectedGameDifficult && selectedGame === game.name) ? true : false} onClick={() => selectGameDifficult("easy")} value="easy" />);
+					btns.push(<Button key="getSelectedGameDifficultMedium" disabled={(selectedGameDifficult && selectedGame === game.name) ? true : false} onClick={() => selectGameDifficult("medium")} value="medium" />);
+					btns.push(<Button key="getSelectedGameDifficultHard" disabled={(selectedGameDifficult && selectedGame === game.name) ? true : false} onClick={() => selectGameDifficult("hard")} value="hard" />);
+				}
+				//if(selectedGame === game.name && selectedGameMode && selectedGameDifficult && gameStatus == "" ) {
+				//	gameInit(game)
+				//}
 
+				return (<GameItem
+						key={game.name}
+						name={game.name}
+						thumb={game.thumb}
+						selected={(selectedGame === game.name) ? true : false}
+					 >{btns}</GameItem>)
+				}
+			)}
+			</GamesList>
+		)
+	}
+}
 
 Game.propTypes = {
   games: PropTypes.arrayOf(PropTypes.shape({
